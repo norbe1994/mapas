842e168b4b490e57c2cfe2abc03552805145b236
Norberto Caceres <norbe1994@yahoo.com>
2019-04-11 09:55:46 -0500

    agregar funcionalidad de agregar marcadores on mapClick

--- src/app/components/mapa/mapa.component.ts
+++ src/app/components/mapa/mapa.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core'
+import { Marcador } from 'src/app/classes/marcador.class'
 
 @Component({
   selector: 'app-mapa',
@@ -6,10 +7,21 @@ import { Component, OnInit } from '@angular/core'
   styleUrls: ['./mapa.component.scss']
 })
 export class MapaComponent implements OnInit {
+  marcadores: Marcador[] = []
+  // Hamm Alemania
   lat: number = 51.678418
   lng: number = 7.809007
 
-  constructor() {}
+  constructor() {
+    const nuevoMarcador = new Marcador(this.lat, this.lng)
+    this.marcadores.push(nuevoMarcador)
+  }
 
   ngOnInit() {}
+
+  agregarMarcador(e: any) {
+    const coords: { lat: number; lng: number } = e.coords
+    const nuevoMarcador = new Marcador(coords.lat, coords.lng)
+    this.marcadores.push(nuevoMarcador)
+  }
 }
